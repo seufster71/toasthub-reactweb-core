3f8b39bc14020e5e528d70ebe5694f738e0a3b83
seufster <seufster@gmail.com>
2018-12-30 21:11:47 -0500

    modal

--- common/list.js
+++ common/list.js
@@ -6,61 +6,6 @@ import Pagination from './pagination';
 
 const List = ({containerState, header, items, itemCount, columns, appPrefs, pageStart, pageLimit,
   onPageLimitChange, onSearchClick, onSearchChange, onPaginationClick, onFilterClick}) => {
-  let listRows = [];
-  let children = true;
-
-  if (columns != null && columns.length > 0) {
-    let headerCell = [];
-      if (children == true){
-        headerCell.push(<div key={0} scope="col"/>);
-      }
-    for (let c = 0; c < columns.length; c++) {
-      let sortOption = <i className="fa fa-unsorted" onClick={onFilterClick(columns[c].name)} />;
-      if (containerState != null && containerState.orderCriteria != null) {
-        let orderCriteria = containerState.orderCriteria;
-        for(let o = 0; o < orderCriteria.length; o++) {
-          if (orderCriteria[o].orderColumn == columns[c].name) {
-            if (orderCriteria.orderDir == "DESC") {
-              sortOption = <i className="fa fa-sort-alpha-desc" onClick={onFilterClick(columns[c].name)} />;
-            } else {
-              sortOption = <i className="fa fa-sort-alpha-asc" onClick={onFilterClick(columns[c].name)} />;
-            }
-          }
-        }
-      }
-
-      headerCell.push(<div key={columns[c].id} scope="col">{columns[c].value} {sortOption}</div>);
-    }
-    listRows.push(<li>{headerCell}</li>);
-  }
-  if (items != null && items.length > 0) {
-    for (let i = 0; i < items.length; i++) {
-      let cells = [];
-      cells.push(<div key={0} scope="row"><i className="fa fa-chevron-right" onClick={onFilterClick(items[i].name)} /></div>);
-      for (let c = 0; c < columns.length; c++) {
-        if (columns[c].optionalParams != null) {
-          let opt = JSON.parse(columns[c].optionalParams);
-          let value = "";
-          if (opt.field != null) {
-            value = items[i][opt.field];
-          } else if (opt.fieldML != null) {
-            value = items[i][opt.fieldML].defaultText;
-          } else if (opt.fieldBool != null) {
-            if (items[i][opt.fieldBool] == true) {
-              value = "Active";
-            }
-          }
-          cells.push(<div key={columns[c].id}>{value}</div>);
-        } else {
-          cells.push(<div key={columns[c].id}/>);
-        }
-      }
-      listRows.push(<li key={items[i].id} >{cells}</li>);
-    }
-  } else {
-    listRows.push(<li key="1"><div id={appPrefs.appTexts.GLOBAL_PAGE.GLOBAL_PAGE_LIST_EMPTY.name}> {appPrefs.appTexts.GLOBAL_PAGE.GLOBAL_PAGE_LIST_EMPTY.value}</div></li>);
-  }
-
 
   return (
       <div className="col-md-12 col-sm-12 col-xs-12">
@@ -68,7 +13,7 @@ const List = ({containerState, header, items, itemCount, columns, appPrefs, page
           <div className="x_title">
             {header.value}
             <ul className="navbar-right panel_toolbox">
-              <li><a className="collapse-link"><i className="fa fa-filter"/></a>
+              <li><a className="collapse-link"><i className="fa fa-filter" onClick={onFilterClick()}/></a>
               </li>
               <li><a className="collapse-link"><i className="fa fa-wrench"/></a>
               </li>
@@ -79,26 +24,10 @@ const List = ({containerState, header, items, itemCount, columns, appPrefs, page
               <ShowEntries name={containerState.pageName+"_PAGELIMIT"} appPrefs={appPrefs} onPageLimitChange={onPageLimitChange}/>
               <Search name={containerState.pageName+"_SEARCH"} onChange={onSearchChange} onClick={onSearchClick} />
             </div>
-            <ul className="list-table table-striped">
-              {listRows}
+            <ul className="list-group">
+              {items}
             </ul>
             <Pagination currentSegment={containerState[containerState.pageName+"_PAGINATION"]} appPrefs={appPrefs} itemCount={itemCount} pageStart={pageStart} pageLimit={pageLimit} onClick={onPaginationClick}/>
-          </div>
-        </div>
-        <div id="filterModal" className="modal fade" role="dialog" aria-labelledby="basicModal">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button type="button" className="close" data-dismiss="modal" aria-hidden="true"><i className="fa fa-close"/></button>
-                <h4 className="modal-title">Modal title</h4>
-              </div>
-              <div className="modal-body">
-                <h3>Modal Body <i className="fa fa-cog fa-spin fa-3x fa-fw"/></h3>
-              </div>
-              <div className="modal-footer">
-                <h3>Footer</h3>
-              </div>
-            </div>
           </div>
         </div>
       </div>
---
+++ common/modal.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+const Modal = ({isOpen, width, height, style, backdropStyle, containerClassName, className, backdropClassName, noBackdrop, onClose, children}) => {
+  if (isOpen === false) {
+    return null;
+  }
+
+  let modalStyle = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '9999'
+  };
+
+  if (width && height) {
+    modalStyle.width = width + 'px';
+    modalStyle.height = height + 'px';
+    modalStyle.marginLeft = '-' + (width/2) + 'px';
+    modalStyle.marginTop = '-' + (height/2) + 'px';
+    modalStyle.transform = null;
+  }
+
+  if (style) {
+    for (let key in style) {
+      modalStyle[key] = style[key];
+    }
+  }
+
+  let backStyle = {
+    position: 'fixed',
+    width: '100%',
+    height: '100%',
+    top: '0px',
+    left: '0px',
+    zIndex: '9998',
+    background: 'rgba(0,0,0,0.3)'
+  };
+
+  if (backdropStyle) {
+    for (let key in backdropStyle) {
+      backStyle[key] = backStyle[key];
+    }
+  }
+
+  return (
+    <div className={containerClassName}>
+      <div className={className} style={modalStyle}>
+        {children}
+      </div>
+      {!noBackdrop && <div className={backdropClassName} style={backStyle}/>}
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  style: PropTypes.object,
+  backdropStyle: PropTypes.object,
+  containerClassName: PropTypes.string,
+  className: PropTypes.string,
+  backdropClassName: PropTypes.string,
+  children: PropTypes.object,
+  noBackdrop: PropTypes.bool,
+  onClose: PropTypes.func
+};
+
+export default Modal;
