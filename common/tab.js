eb9fa40a700f8756b031064132a6a1967ba06cdb
seufster <seufster@gmail.com>
2019-01-13 13:16:33 -0500

    tabs

---
+++ common/tab.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+const Tab = ({activeTab, label, onClick}) => {
+
+  let className = 'tab-list-item';
+
+  if (activeTab === label) {
+    className += ' tab-list-active';
+  }
+
+  return (
+      <li className={className} onClick={onClick}>
+        {label}
+      </li>
+  );
+};
+
+Tab.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Tab;
---
+++ common/tabs.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Tab from './tab';
+
+const Tabs = ({tabLabels, activeTab, onClickTabItem}) => {
+
+
+
+  return (
+    <div className="tabs">
+      <ol className="tab-list">
+        {tabLabels.map((label) => {
+
+          return (
+            <Tab
+              activeTab={activeTab}
+              key={label}
+              label={label}
+              onClick={onClickTabItem}
+            />
+          );
+        })}
+      </ol>
+      <div className="tab-content">
+        No content yet
+      </div>
+    </div>
+  );
+};
+
+Tabs.propTypes = {
+  tabLabels: PropTypes.array.isRequired,
+  activeTab: PropTypes.string.isRequired,
+  onClickTabItem: PropTypes.func.isRequired,
+};
+
+export default Tabs;
