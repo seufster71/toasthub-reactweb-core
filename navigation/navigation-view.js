f1f4914ca2eff2f6026a0486ae9e034b941a3d36
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-08-25 07:36:02 -0400

    menu update

--- navigation/navigation-view.js
+++ navigation/navigation-view.js
@@ -5,60 +5,30 @@ import { IndexLinkContainer, LinkContainer } from "react-router-bootstrap";
 import utils from '../../core/common/utils';
 
 
-export default function NavigationView({menus,appPrefs,permissions,activeTab,changeTab,backToTab,headerToolTip}) {
+export default function NavigationView({menus,appPrefs,permissions,activeTab,changeTab,backToTab,headerToolTip,user,profileMenu}) {
 
     let items = [];
     let topMenus = menus;
 
     if (topMenus != null) {
-      for (let m = 0; m < topMenus.length; m++) {
-        if (permissions != null && !utils.hasPermission(permissions,topMenus[m].permissionCode,"R")) {
-          continue;
-        }
-        if (topMenus[m].values[0].rendered) {
-          let children = [];
-          if (topMenus[m].children != null) {
-            let childList = topMenus[m].children;
-            for (let c = 0; c < childList.length; c++) {
-              if (permissions != null && !utils.hasPermission(permissions,childList[c].permissionCode,"R")) {
-                continue;
-              }
-              if (childList[c].values[0].rendered) {
-                children.push(
-                  <IndexLinkContainer key={topMenus[m].code+"-"+childList[c].menuId} to={childList[c].values[0].href}>
-                  	<MenuItem >{childList[c].values[0].value}</MenuItem>
-                  </IndexLinkContainer>
-                );
-              }
-            }
-          }
-          if (children.length > 0) {
-            items.push(
-              <NavDropdown key={topMenus[m].menuId} title={<span><i className="fa fa-bars fa-1" aria-hidden="true"></i> <span className="navText">{topMenus[m].values[0].value}</span></span>} id={topMenus[m].code} >
-                {children}
-              </NavDropdown>
-            );
-          } else {
-        	let image = "";
-          	if (topMenus[m].values[0].image != null) {
-          		if (topMenus[m].values[0].image.startsWith("fa")) {
-	  	            image = <i className={topMenus[m].values[0].image} aria-hidden="true"/>;
-          		} else {
-	  	            image = <img src={"/img/"+topMenus[m].values[0].image+"_outline.png"} height="20" width="20" />;
-	  	            if (activeTab === topMenus[m].values[0].href) {
-	  	              image = <img src={"/img/"+topMenus[m].values[0].image+".png"} height="25" width="25" />;
-	  	            }
-          		}
-  	        }
-            items.push(
-              <IndexLinkContainer key={topMenus[m].menuId} to={topMenus[m].values[0].href}>
-                 <NavItem>{image}<span className="navText"> {topMenus[m].values[0].value}</span>
-                 </NavItem>
-              </IndexLinkContainer>
-            );
-          }
-        }
-      }
+      items = buildMenu(items,topMenus,permissions);
+    }
+    // user profile
+    if (user != null) {
+    	if (profileMenu != null) {
+    		items = buildMenu(items,profileMenu,permissions,user);
+    	} else {
+		    items.push(
+		        <NavDropdown key="UP" title={<span><i className="fa fa-bars fa-1" aria-hidden="true"></i> <span className="navText">{user.username}</span></span>} id="UP" >
+		        	<IndexLinkContainer key="UP-1" to="/member-profile">
+		        		<MenuItem >Profile</MenuItem>
+		        	</IndexLinkContainer>
+		        	<IndexLinkContainer key="UP-2" to="/member-logout">
+	        			<MenuItem >Logout</MenuItem>
+	        			</IndexLinkContainer>
+		        </NavDropdown>
+		    );
+    	}
     }
     let headerName = "";
     if (appPrefs != null && appPrefs.headerName != "") {
@@ -79,7 +49,81 @@ export default function NavigationView({menus,appPrefs,permissions,activeTab,cha
     );
 }
 
-
+const buildMenu = (items,menus,permissions,user) => {
+	for (let m = 0; m < menus.length; m++) {
+        if (permissions != null && !utils.hasPermission(permissions,menus[m].permissionCode,"R")) {
+          continue;
+        }
+        if (menus[m].values[0].rendered) {
+          let children = [];
+          if (menus[m].children != null) {
+            let childList = menus[m].children;
+            for (let c = 0; c < childList.length; c++) {
+              if (permissions != null && !utils.hasPermission(permissions,childList[c].permissionCode,"R")) {
+                continue;
+              }
+              if (childList[c].values[0].rendered) {
+                children.push(
+                  <IndexLinkContainer key={menus[m].code+"-"+childList[c].menuId} to={childList[c].values[0].href}>
+                  	<MenuItem >{childList[c].values[0].value}</MenuItem>
+                  </IndexLinkContainer>
+                );
+              }
+            }
+          }
+          if (children.length > 0) {
+        	  let value = "";
+            	if (menus[m].optionalParams != null && menus[m].optionalParams != "") {
+            		let optionalParams = JSON.parse(menus[m].optionalParams);
+            		if (optionalParams.override != null && optionalParams.override === "value"){
+            			if (optionalParams.object === "user" && optionalParams.field != null && user != null){
+            				value = user[optionalParams.field];
+            			}
+            		} 
+            	} else {
+            		value = menus[m].values[0].value;
+            	}
+            items.push(
+              <NavDropdown key={menus[m].menuId} title={<span><i className="fa fa-bars fa-1" aria-hidden="true"></i> <span className="navText">{value}</span></span>} id={menus[m].code} >
+                {children}
+              </NavDropdown>
+            );
+          } else {
+        	let image = "";
+          	if (menus[m].values[0].image != null) {
+          		if (menus[m].values[0].image.startsWith("fa")) {
+	  	            image = <i className={menus[m].values[0].image} aria-hidden="true"/>;
+          		} else {
+	  	            image = <img src={"/img/"+menus[m].values[0].image+"_outline.png"} height="20" width="20" />;
+	  	            if (activeTab === menus[m].values[0].href) {
+	  	              image = <img src={"/img/"+menus[m].values[0].image+".png"} height="25" width="25" />;
+	  	            }
+          		}
+  	        }
+          	let value = "";
+          	if (menus[m].optionalParams != null && menus[m].optionalParams != "") {
+          		let optionalParams = JSON.parse(menus[m].optionalParams);
+          		if (optionalParams.override != null && optionalParams.override === "value"){
+          			if (optionalParams.object === "user" && optionalParams.field != null && user != null){
+          				value = user[optionalParams.field];
+          			}
+          		} 
+          	} else {
+          		value = menus[m].values[0].value;
+          	}
+            items.push(
+              <IndexLinkContainer key={menus[m].menuId} to={menus[m].values[0].href}>
+                 <NavItem>{image}<span className="navText"> {value}</span>
+                 </NavItem>
+              </IndexLinkContainer>
+            );
+          }
+        }
+      }
+	return items;
+}
+        
+        
   NavigationView.propTypes = {
     appPrefs: PropTypes.object,
     permissions: PropTypes.object,
@@ -87,5 +131,7 @@ export default function NavigationView({menus,appPrefs,permissions,activeTab,cha
     activeTab: PropTypes.string,
     changeTab: PropTypes.func,
     backToTab: PropTypes.string,
-    headerToolTip: PropTypes.string
+    headerToolTip: PropTypes.string,
+    user: PropTypes.object,
+    profileMenu: PropTypes.array
   };
